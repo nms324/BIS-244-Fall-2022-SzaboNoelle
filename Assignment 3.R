5df6273648a24f9d674a915c53210daa0122074d
Noelle Szabo <nms324@lehigh.edu>
2022-10-06 12:33:26 -0400

    Finished Assignment 3

---
+++ Assignment 3.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(here)
+library(gapminder)
+USC2020<-read.csv(here("covid-19-data","us-counties-2020.csv"))
+USC2021<-read_csv(here("covid-19-data","us-counties-2021.csv"))
+USC2022<-read.csv(here("covid-19-data","us-counties-2022.csv"))
+USCRECENT<-read.csv(here("covid-19-data","us-counties-recent.csv"))
+USLIVE<-read.csv(here("covid-19-data","live","us-counties.csv"))
+
+DATA<-rbind(USC2020,USC2021,USC2022,USCRECENT,USLIVE)
+View(DATA)
+
+PA <- filter(DATA, state=="Pennsylvania" & county=="Lehigh")
+
+PA<-distinct(PA,date,.keep_all = TRUE)
+
+n<-length(PA$date)
+
+PA$incr_cases <- 1
+
+for (i in 2:n) {
+  PA$incr_cases[i] <- (PA$cases[i]-PA$cases[i-1])}
+
+
+
+p<- ggplot(data=PA)
+
+
+p + geom_line(color="blue",mapping = aes(x=date, y=incr_cases),group=1) + 
+  labs(x="Date", y= "New Cases Reported",
+       title = "COVID-19 Cases Reported in
+                                   Lehigh, PA",)
+
